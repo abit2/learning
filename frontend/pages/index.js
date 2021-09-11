f2bd750b583caf18d0a28b1106f7ea637d856125
abit2 <ankit.baruah1@gmail.com>
2021-09-11 22:47:23 +0200

    adding frontend initial commit

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,1 +1,20 @@
-export default () => <div>Welcome to next.js!</div>
+/* pages/index.js */
+import { Button, Alert } from "reactstrap";
+import Layout from "../components/Layout";
+
+const Home = () => {
+    return (
+        <Layout>
+          <div>
+            <div>
+              <Alert color="primary">
+                Hello Project is strapi-next with Bootstrap
+              </Alert>
+              &nbsp; <Button color="primary">Hello from nextjs</Button>
+            </div>
+          </div>
+        </Layout>
+        
+      );
+}
+export default Home;
---
+++ frontend/pages/signin.js
@@ -0,0 +1,8 @@
+
+/* pages/login.js */
+
+const SignIn = () => {
+    return <h1>Sign In</h1>
+};
+
+export default SignIn;
